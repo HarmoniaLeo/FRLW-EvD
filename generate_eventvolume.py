a68e82f8aad1cf724ec1367771c2487b694c03d6
HarmoniaLeo <45067346+HarmoniaLeo@users.noreply.github.com>
2025-05-05 22:42:32 +0900

    Update generate_eventvolume.py

--- generate_eventvolume.py
+++ generate_eventvolume.py
@@ -169,6 +169,7 @@ if __name__ == '__main__':
                 torch.cuda.empty_cache()
             pbar.update(1)
         pbar.close()
-    print("Average Representation time: ")
-    for i,events_window in enumerate(time_windows):
-        print(time_windows, total_time[i] / total_count[i])
+    if mode == "test":
+        print("Average Representation time: ")
+        for i,events_window in enumerate(time_windows):
+            print(time_windows, total_time[i] / total_count[i])
